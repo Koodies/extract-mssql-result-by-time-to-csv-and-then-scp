2416606cd0aa73547526b1853e96ea926ec74bb7
koodies <ah-yoh@hotmail.com>
2021-03-22 10:57:28 +0800

    Fixed tokens and Escape %

--- mssql.bat
+++ mssql.bat
@@ -7,7 +7,7 @@ for /f "delims=" %%y in (counter.txt) do (SET "%%y")
 
 REM fetch the latest ID
 SET QUERY1="set nocount on;select TOP 1 %COUNTER_ID% FROM %QUERY_FROM% ORDER BY %COUNTER_ID% DESC"
-for /f %%a in ('sqlcmd -b -W -S %SQL_MACHINE% -U %SQL_USER% -P %SQL_PASS% -d %SQL_DB% -Q %QUERY1% -h -1') do set LATESTID=%%a
+for /f %%a in ('sqlcmd -b -W -S %SQL_MACHINE% -U %SQL_USER% -P %SQL_PASS% -d %SQL_DB% -Q %QUERY1% -h -1') do SET LATESTID=%%a
 
 if %ROWS% EQU 0 (goto fetch_unlimited) else (goto fetch_with_limit)
 goto Exit
@@ -15,30 +15,30 @@ goto Exit
 
 :fetch_unlimited
 
-set UNTIL=%LATESTID%
+SET UNTIL=%LATESTID%
 
 REM fetch results between the last fetched Id and the latest ID
 SET UNLIMITED="set nocount on;select %QUERY_SELECT% FROM %QUERY_FROM% WHERE %COUNTER_ID% > %LASTID% AND %COUNTER_ID% <= %UNTIL% ORDER BY %COUNTER_ID% ASC"
-sqlcmd -s%delims% -b -W -S %SQL_MACHINE% -U %SQL_USER% -P %SQL_PASS% -d %SQL_DB% -Q %UNLIMITED% | findstr /v /c:"-" /b > "logs.csv"
-if ERRORLEVEL 1 (goto err_handler) else (goto pscp)
+SQLCMD -s%delims% -b -W -S %SQL_MACHINE% -U %SQL_USER% -P %SQL_PASS% -d %SQL_DB% -Q %UNLIMITED% | findstr /v /c:"-" /b > "logs.csv"
+if ERRORLEVEL 1 ( goto err_handler ) else ( goto pscp )
 goto Exit
 
 :fetch_with_limit
 
 REM Check for differences of 1000 between the last ID and latest ID
-set /a DIFF= %LATESTID% - %LASTID%
+SET /a DIFF= %LATESTID% - %LASTID%
 if %DIFF% LSS %ROWS% (set UNTIL=%LATESTID%) else (set /a UNTIL=%LASTID%+%ROWS%)
 
 REM fetch results between the last fetched Id and the latest ID
 SET LIMITED="set nocount on;select TOP %ROWS% %QUERY_SELECT% FROM %QUERY_FROM% WHERE %COUNTER_ID% > %LASTID% AND %COUNTER_ID% <= %UNTIL% ORDER BY %COUNTER_ID% ASC"
-sqlcmd -s%delims% -b -W -S %SQL_MACHINE% -U %SQL_USER% -P %SQL_PASS% -d %SQL_DB% -Q %LIMITED%  | findstr /v /c:"-" /b > "logs.csv"
-if ERRORLEVEL 1 (goto err_handler) else (goto pscp)
+SQLCMD -s%delims% -b -W -S %SQL_MACHINE% -U %SQL_USER% -P %SQL_PASS% -d %SQL_DB% -Q %LIMITED%  | findstr /v /c:"-" /b > "logs.csv"
+if ERRORLEVEL 1 ( goto err_handler ) else ( goto pscp )
 goto Exit
 
 REM Attempt to PSCP
 :pscp
-for /f "tokens=5 delims=|" %%b in ('pscp -P %SCPPORT% -pw %SCPPASS% logs.csv %URL%') do SET PSCP=%%b
-echo %PSCP% | FIND /I "100%">Nul && ( goto updateConfigFile ) || ( goto pscp_handler)
+for /f "tokens=5* delims=|" %%b in ('pscp -P %SCPPORT% -pw %SCPPASS% logs.csv %URL%') do SET PSCP=%%b
+ECHO %PSCP% | FIND /I "100%%">Nul && ( goto updateConfigFile ) || ( goto pscp_handler )
 goto Exit
 
 REM Logging SQL Error into event log
@@ -54,7 +54,7 @@ goto Exit
 REM update and overwrite last id in counter.txt
 :updateConfigFile
 EVENTCREATE /T INFORMATION /ID 999 /L APPLICATION /D "PSCP Successful"
-echo LASTID=%UNTIL% > "counter.txt"
+ECHO LASTID=%UNTIL% > "counter.txt"
 EVENTCREATE /T INFORMATION /ID 999 /L APPLICATION /D "Updated last ID"
 goto Exit
 
